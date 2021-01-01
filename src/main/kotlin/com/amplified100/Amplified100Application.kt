4db7150ed36ac53d69ed3a9c37696db12fb57178
mgaudiophile <mgaudiofile@gmail.com>
2021-01-01 10:42:51 -0700

    initial project commit

---
+++ src/main/kotlin/com/amplified100/Amplified100Application.kt
@@ -0,0 +1,11 @@
+package com.amplified100
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Amplified100Application
+
+fun main(args: Array<String>) {
+    runApplication<Amplified100Application>(*args)
+}
---
+++ src/test/kotlin/com/amplified100/Amplified100ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amplified100
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Amplified100ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
